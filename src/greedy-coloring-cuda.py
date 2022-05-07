5eb224d7de26d208a82f0d0e4ad3d00dfb21ab85
Vitalii Morskyi <vitalij20358@gmail.com>
2022-05-07 14:02:21 +0200

    Document and clean greedy algorithm

--- src/greedy-coloring-cuda.py
+++ src/greedy-coloring-cuda.py
@@ -4,6 +4,7 @@ from numba import cuda
 
 @cuda.jit(device=True, nopython=True)
 def to_zeros(array: np.ndarray):
+    """Fill an array with zeros."""
     for i in range(len(array)):
         array[i] = 0
 
@@ -12,11 +13,8 @@ def to_zeros(array: np.ndarray):
 def vertex_neighbour_colors(graph: np.ndarray,
                             vertex: np.uint32,
                             current_coloring: np.ndarray,
-                            color_occurrences: np.ndarray):
+                            color_occurrences_out: np.ndarray):
     """Counts number of occurrences of each color among vertex neighbours.
-    Data type conventions:
-        - number of vertices = uint32
-        - number of colors = uint16
 
     Parameters
     ----------
@@ -26,31 +24,47 @@ def vertex_neighbour_colors(graph: np.ndarray,
         An array which represents current state of coloring. O means no color is given.
     vertex :
         Vertex which neighbours to search.
-    color_occurrences :
-        ...
+    color_occurrences_out :
+        An output vector, where index represents the color and stored value
+        represents number of occurrences of that color among vertex neighbours.
     """
     for neighbour, edge_with_neighbour in enumerate(graph[vertex]):
         if edge_with_neighbour == 1:
-            color_occurrences[current_coloring[neighbour]] += 1
+            color_occurrences_out[current_coloring[neighbour]] += 1
 
 
 @cuda.jit(device=True, nopython=True)
-def color_single_graph(graph: np.ndarray,
-                       coloring_order: np.ndarray,
-                       coloring_out: np.ndarray):
-    # TODO: docstrings
-    n_colors = np.uint16(0)
+def color_with_single_ordering(graph: np.ndarray,
+                               coloring_order: np.ndarray,
+                               coloring_out: np.ndarray):
+    """Apply greedy coloring using single ordering
+
+    Parameters
+    ----------
+    graph :
+        Adjacency matrix of the graph made up of zeros and ones.
+    coloring_order :
+        The ordering, which is used in the greedy coloring algorithm.
+    coloring_out :
+        An output array with coloring, where index represents the vertex number
+        and stored value represents color of that vertex.
+    """
+    n_colors = 0
     for vertex in coloring_order:
-        # TODO: replace exact values with variables (cuda.local.array)
-        occupied_colors = cuda.local.array(7, dtype=np.uint32)
+
+        # which colors has already been occupied by vertex neighbours?
+        occupied_colors = cuda.local.array(N_VERTICES, dtype=np.uint32)
         to_zeros(occupied_colors)
         vertex_neighbour_colors(graph, vertex, coloring_out, occupied_colors)
+
+        # find the smallest unused color
         new_color = 0
         for color in range(1, n_colors + 2):
             if occupied_colors[color] == 0:
                 new_color = np.uint16(color)
                 break
         if new_color == n_colors + 1:
+            # new color has been used, increase counter
             n_colors += 1
         coloring_out[vertex] = new_color
 
@@ -59,6 +73,20 @@ def color_single_graph(graph: np.ndarray,
 def color_graphs(graph: np.ndarray,
                  coloring_orders: np.ndarray,
                  colorings_out: np.ndarray):
+    """Apply greedy coloring for many orderings at the same time.
+    Data type conventions:
+        - number of vertices = uint32
+        - number of colors = uint16
+
+    Parameters
+    ----------
+    graph :
+        Adjacency matrix of the graph made up of zeros and ones.
+    coloring_orders :
+        An array of coloring orders.
+    colorings_out :
+        An output array of colorings for each order.
+    """
     tx = cuda.threadIdx.x
     bx = cuda.blockIdx.x
 
@@ -68,8 +96,10 @@ def color_graphs(graph: np.ndarray,
     start = tx + bx * block_size
     stride = block_size * grid_size
 
+    to_zeros(colorings_out)
+
     for i in range(start, coloring_orders.shape[0], stride):
-        color_single_graph(graph, coloring_orders[i], colorings_out[i])
+        color_with_single_ordering(graph, coloring_orders[i], colorings_out[i])
 
 
 A = np.array([
@@ -79,6 +109,9 @@ A = np.array([
     [0, 1, 1, 0, 1],
     [1, 1, 0, 1, 0],
 ], dtype=np.int8)
+N_VERTICES = A.shape[0]
+N_EDGES = np.sum(A)
+
 C = np.array([1, 2, 3, 1, 3], dtype=np.uint16)
 order0 = np.array([3, 1, 4, 0, 2], dtype=np.uint32)
 order1 = np.arange(5, dtype=np.uint32)
@@ -94,5 +127,3 @@ blocks_per_grid = 30
 
 color_graphs[blocks_per_grid, threads_per_block](A, orders, colorings)
 print(colorings)
-
-
